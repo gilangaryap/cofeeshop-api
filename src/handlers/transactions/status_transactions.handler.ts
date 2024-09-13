fcb227836aa5cfd1ecddc6784177ed3024f6fe6a
gilangaryap <gilang2001.ap@gmail.com>
2024-09-14 00:06:33 +0700

    Updated unit tests to cover new error handling cases

--- src/handlers/transactions/status_transactions.handler.ts
+++ src/handlers/transactions/status_transactions.handler.ts
@@ -10,15 +10,23 @@ export const create = async (req: Request<IStatusTransactionBody> ,res: Response
             data: result.rows,
         });
     }catch (err: unknown) {
-        if (err instanceof Error) {
-            console.log(err.message);
+      let errorMessage = "Internal Server Error";
+      if (err instanceof Error) {
+          errorMessage = err.message;
+          if (errorMessage.includes('duplicate key value violates unique constraint "unique_status"')) {
+              errorMessage = "Duplicate status transction name";
+              return res.status(400).json({
+                  msg: "Error",
+                  err: errorMessage,
+              });
           }
-          return res.status(500).json({
-            msg: "Error",
-            err: "Internal Server Error",
-          });
-        }  
-}
+      }
+      return res.status(500).json({
+          msg: "Error",
+          err: errorMessage,
+      });
+  }
+};
 
 export const FetchAll = async (req: Request ,res: Response) => {
     try{
